514a04e834c950ef231bf1faa55688dcac28d86a
Hack Reactor Student <student@hackreactor.com>
2017-10-09 12:08:44 -0700

    Add className search

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,25 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, node) {
+  //document.body, element.childNodes, element.classList
+  //variable for an array to return the className
+  var nodes = [];
+  //set default value for node
+  node = node || document.body;
+
+  //create a condition to check for classNames
+  for (var i = 0; i < node.classList.length; i++) {
+    if (node.classList === className) {
+  //if it exists push to array
+      nodes.push(node);
+    }
+  }  
+
+  
+  //if the parent has children
+  //then iterate through children
+  //then return to top and run the check for className
+
+  //return array
 };
