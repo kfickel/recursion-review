5edaa90b84784e3e0f128d21fa27a31bf83ca80f
Hack Reactor Student <student@hackreactor.com>
2017-10-09 15:42:29 -0700

    Stringify strings, objects, and arrays

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,32 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  //convert strings into strings
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  //convert arrays into string
+  if (Array.isArray(obj)) {
+    //var to store obj array results
+    var stringifiedResults = [];
+    for (var i = 0; i < obj.length; i++) {
+      stringifiedResults.push(stringifyJSON(obj[i]));
+    }
+    //convert arrays within something
+    return '[' + stringifiedResults + ']';
+  }
+  //turn objects into strings
+  if (typeof obj === 'object' && obj !== null) {
+    //convert objects within something
+    var result = [];
+    for (var key in obj) {
+      if (!typeof obj[key] === undefined || 'function') {
+        result.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+      }
+    }
+    return '{' + result.join(',') + '}';
+  }
+  //add ' ' to obj input to convert obj input into strings for numbers,etc.
+  return '' + obj;
 };
